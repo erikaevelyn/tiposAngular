908ff964972deae21ef6b4e20f59f7e8d9cb24b7
erikaevelyn <erikaevelynrodriguez@gmail.com>
2019-07-29 20:16:27 -0300

    Add files via upload

---
+++ Codigo.ts
@@ -0,0 +1,13 @@
+let s: String = "Coco";
+s = "100"; 
+
+function suma (numero: number, numero2: number){
+    return numero + numero2;
+}
+
+let resultado:number = suma(3,2);
+let resultado2:number = suma(3,2);
+
+let a: boolean = true;
+let ob: any = true;
+let ingredientes: Array<string>;
